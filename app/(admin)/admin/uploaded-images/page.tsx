a054f342505e428e76fbb1a846466a7fdad01bde
Ole Tjernstad <ott556@gmail.com>
2025-06-05 22:10:04 +0200

    add revalidation

--- app/(admin)/admin/uploaded-images/page.tsx
+++ app/(admin)/admin/uploaded-images/page.tsx
@@ -1,6 +1,8 @@
 import { UploadedImagesGallery } from "@/components/admin/uploaded-images-gallery";
 import { getUploadedImagesGroupedByBatch } from "./uploaded-images.actions";
 
+export const revalidate = 1800; // revalidate at most every 30 minutes
+
 export default async function AdminUploadedImagesPage() {
   const batches = await getUploadedImagesGroupedByBatch();
   return <UploadedImagesGallery batches={batches} />;
